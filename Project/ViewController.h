ba29b161ceb2fffd55b995b77d84707734c99ab0
Viktor Fefelov <viktorfefelov@Viktors-MacBook-Pro.local>
2012-10-12 13:16:21 +0200

    Initial Commit

---
+++ Project/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project
+//
+//  Created by Viktor Fefelov on 12.10.12.
+//  Copyright (c) 2012 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
