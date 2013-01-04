7e70f5054f93611a5b4ccfbdfc94da2d8c89ff0d
ViktorFefelov <viktorfefelov@gmail.com>
2013-01-04 18:33:59 +0100

    first Commit

---
+++ Project/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  Project
+//
+//  Created by Viktor Fefelov on 12.10.12.
+//  Copyright (c) 2012 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UITableViewController{
+    NSArray *list;
+}
+
+@property NSArray *list;
+
+@end
